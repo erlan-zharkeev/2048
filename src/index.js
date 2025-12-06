3a7c383267550541f5bbf7ebcb0d75f8bc5dc762
Erlan <zharkeev.post@gmail.com>
2025-12-06 20:44:04 +0500

    fix redirect error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './js/game'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {})
+    navigator.serviceWorker.register('./sw.js').catch(() => {})
   })
 }
