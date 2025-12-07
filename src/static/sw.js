7da980bec3194a1131074317a232af555065e328
Erlan <zharkeev.post@gmail.com>
2025-12-07 12:49:19 +0500

    update sw cache

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,34 +1,33 @@
-const CACHE_NAME = '2048-cache-v1';
+const CACHE_NAME = '2048-v2.2.6';
 const ASSETS = [
-  './',
-  './site.webmanifest',
-  './apple-touch-icon.png',
-  './android-chrome-192x192.png',
-  './android-chrome-256x256.png',
-  './favicon-32x32.png',
-  './favicon-16x16.png'
+  '/site.webmanifest',
+  '/apple-touch-icon.png',
+  '/android-chrome-192x192.png',
+  '/android-chrome-256x256.png',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+  const url = new URL(event.request.url);
+
+  if (ASSETS.includes(url.pathname)) {
+    event.respondWith(
+      caches.match(event.request).then(c => c || fetch(event.request))
+    );
+  }
 });
